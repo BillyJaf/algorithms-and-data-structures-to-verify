6d3065b35996656c3016ed580ecd0b5e30054d42
Billy Jaffray <billyjaf@outlook.com>
2026-02-07 14:19:04 +1030

    feat: concurrent mergesort with num_thread threshold for i32

--- src/merge_sort/merge_sort_concurrent.rs
+++ src/merge_sort/merge_sort_concurrent.rs
@@ -1,33 +1,73 @@
-use crate::merge_sort::merge_sort_sequential::merge_sort_sequential;
 use std::thread;
 
-pub fn merge_sort_concurrent<T>(arr: &mut [T], num_threads: usize)
-    where T: Eq + PartialOrd + Copy + Send
-{
-    let mut rounded_up_power_of_two_length = 1;
-    while rounded_up_power_of_two_length < arr.len() {
-        rounded_up_power_of_two_length *= 2;
+use super::merge_sort_sequential::merge_sort_sequential;
+
+pub fn merge_sort_concurrent(arr: &mut [i32], num_threads: usize) {
+    let mut power_of_two = 1;
+    let mut threshold = 0;
+    while power_of_two < num_threads {
+        power_of_two *= 2;
+        threshold += 1;
+    }
+    merge_sort(arr, threshold);
+}
+
+fn merge_sort(arr: &mut [i32], threshold: usize) {
+    let mid_point = arr.len() / 2;
+
+    if mid_point == 0 {
+        return;
     }
 
-    let chunk_size = rounded_up_power_of_two_length / num_threads;
-    let handles = Vec::with_capacity(num_threads);
-
-    for i in 0..(num_threads-1) {
-        let (left, right) = arr.split_at_mut(i*chunk_size);
-        handles.push(
-            thread::spawn(move || {
-                merge_sort_sequential(left);
-                return left;
-            })
-        );
-        arr = right;
+    if threshold == 0 {
+        merge_sort_sequential(arr);
+        return;
     }
 
-    handles.push(
-        thread::spawn(move || {
-            merge_sort_sequential(arr);
-            return arr;
-        })
-    );
+    let (left, right) = arr.split_at_mut(mid_point);
 
+    let mut left_clone = Vec::from(left);
+    let mut right_clone = Vec::from(right);
+
+    let left_handle = thread::spawn(move || {
+        merge_sort(&mut left_clone, threshold - 1);
+        return left_clone;
+    });
+
+    let right_handle = thread::spawn(move || {
+        merge_sort(&mut right_clone, threshold - 1);
+        return right_clone;
+    });
+
+    let mut left_sorted = left_handle.join().unwrap();
+    let mut right_sorted = right_handle.join().unwrap();
+    let mut temp_vec = Vec::with_capacity(left_sorted.len() + right_sorted.len());
+
+    merge(&mut left_sorted, &mut right_sorted, &mut temp_vec);
+    arr.copy_from_slice(&temp_vec);
+}
+
+fn merge(left: &mut [i32], right: &mut [i32], temp_vec: &mut Vec<i32>) {   
+    let mut left_pointer = 0;
+    let mut right_pointer = 0;
+
+    while left_pointer < left.len() && right_pointer < right.len() {
+        if left[left_pointer] <= right[right_pointer] {
+            temp_vec.push(left[left_pointer]);
+            left_pointer += 1;
+        } else {
+            temp_vec.push(right[right_pointer]);
+            right_pointer += 1;
+        }
+    }
+
+    while left_pointer < left.len() {
+        temp_vec.push(left[left_pointer]);
+        left_pointer += 1;
+    }
+
+    while right_pointer < right.len() {
+        temp_vec.push(right[right_pointer]);
+        right_pointer += 1;
+    }
 }
--- src/merge_sort/tests.rs
+++ src/merge_sort/tests.rs
@@ -2,23 +2,17 @@ use crate::merge_sort::merge_sort_sequential::merge_sort_sequential;
 use crate::merge_sort::merge_sort_concurrent::merge_sort_concurrent;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
+use std::time::Instant;
 
 #[test]
-fn simple_sort_sequential() {
+fn small_sort_sequential() {
     let mut arr = vec![2,8,0,4,0,3];
     merge_sort_sequential(&mut arr);
     assert_eq!(arr, vec![0,0,2,3,4,8]);
 }
 
 #[test]
-fn longer_sort_sequential() {
-    let mut arr = vec![15,13,8,9,10,12,14,0,1,2,3,6,5,7,4,11];
-    merge_sort_sequential(&mut arr);
-    assert_eq!(arr, vec![0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
-}
-
-#[test]
-fn large_sort_sequential() {
+fn medium_sort_sequential() {
     let mut arr_shuffled: Vec<i32> = (1..=1024).collect();
     let mut rng = thread_rng();
     arr_shuffled.shuffle(&mut rng);
@@ -30,8 +24,71 @@ fn large_sort_sequential() {
 }
 
 #[test]
+fn large_sort_sequential() {
+    let mut arr_shuffled: Vec<i32> = (1..=65536).collect();
+    let mut rng = thread_rng();
+    arr_shuffled.shuffle(&mut rng);
+    merge_sort_sequential(&mut arr_shuffled);
+
+    let arr_sorted: Vec<i32> = (1..=65536).collect();
+
+    assert_eq!(arr_shuffled, arr_sorted);
+}
+
+#[test]
 fn simple_sort_concurrent() {
     let mut arr = vec![2,8,0,4,0,3];
-    merge_sort_concurrent(&mut arr, 8);
+    merge_sort_concurrent(&mut arr, 2);
     assert_eq!(arr, vec![0,0,2,3,4,8]);
+}
+
+#[test]
+fn medium_sort_concurrent() {
+    let mut arr_shuffled: Vec<i32> = (1..=1024).collect();
+    let mut rng = thread_rng();
+    arr_shuffled.shuffle(&mut rng);
+    merge_sort_concurrent(&mut arr_shuffled, 16);
+
+    let arr_sorted: Vec<i32> = (1..=1024).collect();
+
+    assert_eq!(arr_shuffled, arr_sorted);
+}
+
+#[test]
+fn large_sort_concurrent() {
+    let mut arr_shuffled: Vec<i32> = (1..=65536).collect();
+    let mut rng = thread_rng();
+    arr_shuffled.shuffle(&mut rng);
+
+    merge_sort_concurrent(&mut arr_shuffled, 16);
+
+    let arr_sorted: Vec<i32> = (1..=65536).collect();
+
+    assert_eq!(arr_shuffled, arr_sorted);
+}
+
+
+#[test]
+fn compare_large_sort() {
+    let mut arr_shuffled_one: Vec<i32> = (1..=65536).collect();
+    let mut rng = thread_rng();
+    arr_shuffled_one.shuffle(&mut rng);
+    let mut arr_shuffled_two = arr_shuffled_one.clone();
+
+    let start = Instant::now();
+    merge_sort_sequential(&mut arr_shuffled_one);
+    let elapsed = start.elapsed();
+
+    println!("Large sequential sort took: {:?}", elapsed);
+
+    let start = Instant::now();
+    merge_sort_concurrent(&mut arr_shuffled_two, 16);
+    let elapsed = start.elapsed();
+
+    println!("Large concurrent sort took: {:?}", elapsed);
+
+    let arr_sorted: Vec<i32> = (1..=65536).collect();
+
+    assert_eq!(arr_shuffled_one, arr_sorted);
+    assert_eq!(arr_shuffled_two, arr_sorted);
 }
