70269707a8ae683adb363eefe07c7bc995901285
Billy Jaffray <billyjaf@outlook.com>
2026-02-07 17:36:11 +1030

    chore: changed vec to owned and returned for verus semantics

--- src/insertion_sort/insertion_sort.rs
+++ src/insertion_sort/insertion_sort.rs
@@ -1,10 +1,10 @@
-pub fn insertion_sort(arr: &mut Vec<i32>) {
+pub fn insertion_sort(mut arr: Vec<i32>) -> Vec<i32> {
     let num_elements = arr.len();
     
     // Do this check so that later we can cast to an i32 safely.
     if num_elements > (u32::MAX >> 1) as usize {
         println!("Array is too large to sort. Array length: {}", arr.len());
-        return
+        return arr;
     }
 
     for i in 1..num_elements {
@@ -17,4 +17,5 @@ pub fn insertion_sort(arr: &mut Vec<i32>) {
 
         arr[(j + 1) as usize] = current_element;
     }
+    return arr;
 }
